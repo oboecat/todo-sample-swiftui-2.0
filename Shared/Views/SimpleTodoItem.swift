cb8ee824fba89abfe7b73783e1ad65b57aeb25e1
Liliya Pustovoyt <lila@pustovoyt.me>
2020-07-10 19:25:38 -0400

    Removed Pods

---
+++ Shared/Views/SimpleTodoItem.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleTodoItem.swift
+//  NewTodoSample
+//
+//  Created by Lila Pustovoyt on 7/9/20.
+//
+
+import SwiftUI
+
+struct SimpleTodoItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SimpleTodoItem_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleTodoItem()
+    }
+}
